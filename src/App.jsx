6c679bb360322404dc35bc0e9deb7413548cced0
SuchakreeS <poomfail2013@gmail.com>
2026-03-13 11:34:02 +0700

    added daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="text-4xl font-extrabold">
+        Fakebook CC22
+      </div>
+      <button className="btn btn-primary">This is button</button>
+    </>
+  )
+}
+
+export default App
