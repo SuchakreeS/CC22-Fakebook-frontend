518a61ada3e1c66a36e198a58aa6649f5baacc0f
SuchakreeS <poomfail2013@gmail.com>
2026-03-16 14:44:50 +0700

    connect fornt to back & register added

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,58 +1,94 @@
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { registerSchema } from "@/validations/schema"
+import axios from "axios"
+import { Flip, toast, ToastContainer, Zoom } from "react-toastify"
 
 
 function RegisterForm() {
     const { register, handleSubmit, formState, reset } = useForm({
         resolver: zodResolver(registerSchema),
         mode: 'onSubmit',
-        dedefaultValues: {
-            firstName: '', lastName: '', identity: '', password: ''
+        defaultValues: {
+            firstName: '', lastName: '', identity: '', password: '', confirmPassword: ''
         }
     })
-    const { errors } = formState
+    const { errors, isSubmitting } = formState
 
-    const onSubmit = (data) => {
+    const onSubmit = async (data) => {
         try {
-            alert(JSON.stringify(data, null, 2))
+            await new Promise((resolve) =>
+                setTimeout(resolve, 2000))
+            const resp = await axios.post('http://localhost:8899/api/auth/register', data)
+            document.getElementById('register-form').close()
+            toast.success(JSON.stringify(resp.data.message), { transition: Zoom, autoClose: 4000 })
+            reset()
         } catch (err) {
-            // alert(JSON.stringify(err, null, 2))
-            console.log(err)
+            console.dir(err)
+            const errMsg = err.response?.data.message || err.message
+            // alert(JSON.stringify(err,null,2))
+            toast.error(errMsg, {
+                transition: Zoom, autoClose: 3000, containerId: 'register-modal', position: 'top-center'
+            })
         }
-
     }
     return (
         <>
-            <div className="text-3xl text-center opacity-60">
-                Create a new account
+            <ToastContainer containerId="register-modal" />
+            <div className="text-3xl text-center opacity-70">Create a new account
+                {isSubmitting && <span className="loading loading-infinity  text-info mx-2"></span>}
             </div>
-            <div className="divider opcaity-60"></div>
-            <form onSubmit={handleSubmit(onSubmit)}
-                className='flex flex-col gap-5 p-4 pt-3'
-            >
-                <div className="felx gap-2">
-                    <input type="text" className="input w-full " placeholder='First name' {...register('firstName')} />
-                    <input type="text" className="input w-full" placeholder='Last name'{...register('lastName')} />
-                </div>
-                <input type="text" className="input w-full" placeholder='Email or Phone number'{...register('identity')} />
-                <input type="password" className="input w-full"
-                    placeholder='New password' {...register('password')}
-                />
-                <input type="password" className="input w-full"
-                    placeholder='Confirm password' {...register('confirmPassword')}
-                />
-                <button className="btn btn-secondary text-xl">
-                    Sign up
-                </button>
+            <div className="divider opacity-60"></div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <fieldset className='flex flex-col gap-5 p-4 pt-3' disabled={isSubmitting}>
+                    <div className="flex gap-2">
+                        <div className="w-full">
+                            <input type="text" className="input w-full"
+                                placeholder='First name'
+                                {...register('firstName')}
+                            />
+                            <p className="text-sm text-error">{errors.firstName?.message}</p>
+                        </div>
+                        <div className="w-full">
+                            <input type="text" className="input w-full"
+                                placeholder='Last name'
+                                {...register('lastName')}
+                            />
+                            <p className="text-sm text-error">{errors.lastName?.message}</p>
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <input type="text" className="input w-full"
+                            placeholder='Email or Phone number'
+                            {...register('identity')}
+                        />
+                        <p className="text-sm text-error">{errors.identity?.message}</p>
+                    </div>
+                    <div className="w-full">
+                        <input type="password" className="input w-full"
+                            placeholder='New password'
+                            {...register('password')}
+                        />
+                        <p className="text-sm text-error">{errors.password?.message}</p>
+                    </div>
+                    <div className="w-full">
+                        <input type="password" className="input w-full"
+                            placeholder='Confirm password'
+                            {...register('confirmPassword')}
+                        />
+                        <p className="text-sm text-error">{errors.confirmPassword?.message}</p>
+                    </div>
+                    <button className="btn btn-secondary text-xl" disabled={isSubmitting}>Sign up</button>
+                    {/* ทำปุ่มนี้ให้ clear form ได้ */}
+                    <button type='button' className="btn btn-base-200 text-xl" onClick={() => reset()} >Clear</button>
+                </fieldset>
             </form>
-            <div className="border">
-                <pre className="text-error text-xs">
-                    {JSON.stringify(errors, (k, v) => k === 'ref' ? undefined : v, 2)}</pre>
-            </div>
+            {/* <div className="border">
+        <pre className="text-error text-xs">
+          {JSON.stringify(errors, (k, v) => k === 'ref' ? undefined : v, 2)}
+        </pre>
+      </div> */}
         </>
-
     )
 }
-
 export default RegisterForm
